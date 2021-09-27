7cf2f054e39279a83b2dbed470549673e83e4e37
Richard Vowles <richard@bluetrainsoftware.com>
2021-09-27 20:45:57 +1300

    fixing the io/html issue (#4)

--- client-dart-sdk/test/src/sse_client_test.dart
+++ client-dart-sdk/test/src/sse_client_test.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:featurehub_client_api/api.dart';
 import 'package:featurehub_client_sdk/featurehub.dart'
     hide EventSourceRepositoryListener;
-import 'package:featurehub_client_sdk/featurehub_io.dart';
+import 'package:featurehub_client_sdk/featurehub.dart';
 import 'package:featurehub_sse_client/featurehub_sse_client.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rxdart/rxdart.dart';
