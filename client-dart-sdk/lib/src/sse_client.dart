6822707662e82df74c93258b7d58dcd34ffa30d4
Richard Vowles <richard@bluetrainsoftware.com>
2021-09-27 20:35:22 +1300

    fixing the io/html issue

--- client-dart-sdk/lib/src/sse_client.dart
+++ client-dart-sdk/lib/src/sse_client.dart
@@ -3,6 +3,15 @@ import 'repository.dart';
 /// this is a stub implementation, you shouldn't get it if you use dart:io or dart:html
 /// which is pretty much every option
 class EventSourceRepositoryListener {
-  EventSourceRepositoryListener(String hostUrl, String apiKey, ClientFeatureRepository repository,
+  EventSourceRepositoryListener(
+      String hostUrl, String apiKey, ClientFeatureRepository repository,
       {bool doInit = true});
+
+  void close() {
+    throw UnimplementedError('This is implemented in the concrete version');
+  }
+
+  Future<void> init() {
+    throw UnimplementedError('This is implemented in the concrete version');
+  }
 }
--- client-dart-sdk/other_examples/dart_cli/main.dart
+++ client-dart-sdk/other_examples/dart_cli/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:featurehub_client_api/api.dart';
-import 'package:featurehub_client_sdk/featurehub_io.dart';
+import 'package:featurehub_client_sdk/featurehub.dart';
 import 'package:logging/logging.dart';
 
 void main() async {
